c66855b7960401c852a5678c255e34d70e720540
Junior Francisco Garcia Ayala <junior.f.garcia99@gmail.com>
2019-12-15 20:13:32 -0500

    first deploy

--- food_spin/food_spin/settings.py
+++ food_spin/food_spin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'spin'
 LOGOUT_REDIRECT_URL = 'login'
+
+
+django_heroku.settings(locals())
